176a3cd591512aeebcaac71a4e719480307ef8fb
unknown <balvalloire@gmail.com>
2021-04-14 17:54:40 +0200

    :sprakles: add scan qrcode

--- src/main.js
+++ src/main.js
@@ -11,6 +11,20 @@ class App {
     initApp () {
         new Socket()
         new PwaPopUp()
+
+        function onScanSuccess(qrMessage) {
+            // handle the scanned code as you like
+            console.log(`QR matched = ${qrMessage}`);
+        }
+
+        function onScanFailure(error) {
+            // handle scan failure, usually better to ignore and keep scanning
+            console.warn(`QR error = ${error}`);
+        }
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", { fps: 10, qrbox: 250 }, /* verbose= */ true);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
     }
 
     initServiceWorker () {
