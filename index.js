6c652a14c1413cd67921d95560187c1b2f9890b8
unknown <balvalloire@gmail.com>
2021-04-20 20:42:48 +0200

    :construction: theme load

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const port = process.env.PORT || 3003;
 
 app.set('view engine', 'ejs');
 
+const theme = [
+    {
+      "title" : "L'épilation",
+      "img" : "../../assets/images/themes/epilation.jpg"
+    },
+  {
+    "title" : "Les régles",
+    "img" : "../../assets/images/themes/regles.jpg"
+  }
+  ]
 
 /*
 * Redirect to https
@@ -21,6 +31,15 @@ app.get('/', function(req, res) {
 app.get('/views/pages/scan.ejs', function(req, res) {
   res.render('pages/scan');
 });
+
+app.get('/views/pages/theme.ejs', function(req, res, next) {
+  res.render('pages/theme', {
+    query : req.query,
+    theme
+  });
+});
+
+
 
 app.all("*", function (req, resp, next) {
   if (req.params[0].substr(-5,5) === '.html') return
