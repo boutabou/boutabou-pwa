5279dafb9ecf926a6ced1f2a214d2fba8e09816f
unknown <balvalloire@gmail.com>
2021-05-04 15:28:13 +0200

    :construction: getUsersDashboard

--- server/utils.js
+++ server/utils.js
@@ -36,11 +36,10 @@ function getTheme(socket) {
 function getUsersWithDashboard(users, currentTheme) {
     const interaction = require('./' + currentTheme.pathInteractions)
 
+    delete require.cache[require.resolve('./data/tables.json')]
+    const dashboard = require('./data/tables.json')
+
     users.forEach((user) => {
-
-        delete require.cache[require.resolve('./data/tables.json')];
-        const dashboard = require('./data/tables.json')
-
         user.dashboard = dashboard[Math.floor((Math.random() * dashboard.length))]
 
         user.dashboard.forEach((interactionBoard) => {
