b35032c6615105d4569cbcd6c6a65bea0126fc12
Maeva Reynaud <maeva.reynaud@hotmail.fr>
2021-05-07 14:46:50 +0200

    :sparkles: rotate words

---
+++ src/scripts/utils/SplitContent.js
@@ -0,0 +1,30 @@
+import gsap from "gsap"
+import { SplitText } from "gsap/SplitText"
+
+export default class SplitContent {
+    constructor() {
+        this.initEls()
+        this.initEvents()
+    }
+
+    initEls() {
+        this.$els = {
+            texts: document.querySelectorAll('.js-split-text')
+        }
+    }
+
+    initEvents() {
+        if(this.$els.texts){
+            this.splitText()
+        }
+    }
+
+    splitText () {
+        gsap.registerPlugin(SplitText) 
+
+        this.$els.texts.forEach((text, index) => {
+            this.$els.texts[index].setAttribute("id", "splitText-"+index)
+            new SplitText("#splitText-"+index, {type:"words,chars"})
+        });   
+    }
+}
